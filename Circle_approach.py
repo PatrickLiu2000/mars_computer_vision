30bdd11b357b85a1de45361dd2752d3bed6dad94
joey <josephcrawford99@gmail.com>
2020-10-27 23:02:23 -0400

    Circle detector at least works now

--- Circle_approach.py
+++ Circle_approach.py
@@ -1,10 +1,16 @@
 import numpy as np
+import os
 from scipy import ndimage
 from skimage.color import rgb2gray
 from skimage.feature import canny
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+from scipy.spatial import distance
 
+path = os.getcwd()
+
+assetsdir = path + "/assets/"
+testmap= plt.imread(assetsdir+"thm_dir_N-30_060sample.png")
 def get_gradients(imggray: np.ndarray):
     Mx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
     My = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
@@ -14,32 +20,55 @@ def get_gradients(imggray: np.ndarray):
     gx = ndimage.correlate(img64, Mx, mode='constant', cval=0.0)
     gy = ndimage.correlate(img64, My, mode='constant', cval=0.0)
     return gx, gy
+def next_to_black(x, y, imggray):
+    if imggray[y][x+1] ==0 or imggray[y][x-1]==0 or imggray[y+1][x]==0 or imggray[y-1][x]==0:
+        print("true")
+        return True
+    else:
+        return False
 
-def detect_circles(img: np.ndarray, radius:int, use_gradient=True, denoise=False, quant = False):
-    sig = 2#can be changed based on image, sigma for canny edge detector
-    numcircles = 1 #Can be changed to reflect any number of circles
-    centers = np.empty((0,2)) #the return value, a list of coordinates for circle centers
+def detect_circles(img: np.ndarray, radius:int,use_gradients = True, denoise=False, allow_overlap =False):
+    sig = 3  #can be changed based on image, sigma for canny edge detector
+    numcircles = 3 #Can be changed to reflect any number of circles
+
     gray = rgb2gray(img)
+
     edges = canny(image=gray,sigma=sig)
+
     edgePixels = np.where(edges ==True)
+    # print("ED pix format", edgePixels)
     edgeIndex = np.zeros((len(edgePixels[0]),2))
+
+
     for i in range(len(edgeIndex)):
         edgeIndex[i] = [edgePixels[0][i], edgePixels[1][i]]
     edgeIndex = edgeIndex.astype(np.uint32)
+    # for j in range(len(edgeIndex)):
+    #     print(edgeIndex[j][1], edgeIndex[j][0])
+    #     if next_to_black(edgeIndex[j][1], edgeIndex[j][0], gray):
+    #         edges[edgeIndex[j][0]][edgeIndex[j][1]] = 0
+    #         print("made it here")
+    #         edgeIndex = np.delete(edgeIndex, j) #remove edges caused by missing photos
+
+    plt.imshow(edges)
+    plt.show()
+
+
     H = np.zeros((img.shape[0], img.shape[1])) #the hough accumulator array
 
     thetas = range(360)
     thetas = np.radians(thetas)
     theta = np.zeros_like(H)
 
-
-    gx, gy = get_gradients(imggray=gray)
-    for i in edgeIndex:
-        theta[i[0],i[1]] = np.arctan2(float(gy[i[0], i[1]]), float(gx[i[0], i[1]]))
-
+    if use_gradients:
+        gx, gy = get_gradients(imggray=gray)
+        for i in edgeIndex:
+            theta[i[0],i[1]] = np.arctan2(float(gy[i[0], i[1]]), float(gx[i[0], i[1]]))
 
     for pixel in edgeIndex:
-        thetas = [theta[pixel[0]][pixel[1]]]
+        # print("On pixel ", pixel, " of ", len(edgeIndex))
+        if use_gradients:
+            thetas = [theta[pixel[0]][pixel[1]]]
         for t in thetas:
             a=pixel[1] + (radius*np.cos(t))
             b=pixel[0] + (radius*np.sin(t))
@@ -57,22 +86,44 @@ def detect_circles(img: np.ndarray, radius:int, use_gradient=True, denoise=False
         Hnew = (H >= thresh) * H
         H = Hnew
         numcircles = int(len(np.where(H != 0))/2)
-
+    centers = []  # the return value, a list of coordinates for circle centers
+    Htemp = np.copy(H)
     for i in range(numcircles):
-        max = np.amax(H)
-        center = np.where(H == max)
-        center = [center[0][0], center[1][0]]
-        centers = np.append(centers, (center, radius), axis=0) #centers holds location and radius
-        H[center[0]][center[1]] = -1 #to let us find a different center
+        max = np.amax(Htemp)
+        print("Max votes was ", max)
+        center = np.where(Htemp == max)
+        print("center is located ", center )
+        center = [center[1][0], center[0][0]]
+        centers.append((center, radius)) #centers holds location and radius
+        Htemp[center[1]][center[0]] = -1 #to let us find a different center
+        if not allow_overlap:
+            for row in range(len(Htemp)):
+                for col in range(len(Htemp[0])):
+                    point = (col, row)
+                    if distance.euclidean(point, center) < (2*radius):
+                        Htemp[point[1]][point[0]] = -1
 
-    return centers
+
+
+    return centers, H
+
 
 def display(H, centers, img):
-    fig, axs = plt.subplots(2)
+    print("displaying")
+
+    fig, axs = plt.subplots(3)
     axs[1].imshow(H)
     for i in centers:
-        circ = Circle((i[0][1], i[0][0]), i[1], color = 'red',fill=False)
+        circ = Circle(i[0], i[1], color = 'red',fill=False)
         axs[0].add_artist(circ)
     axs[0].imshow(img)
+    sig=3
+    gray = rgb2gray(img)
+    axs[2].imshow(canny(image=gray,sigma=sig))
     plt.show()
+
     # axs[0].set_title("Radius = 100")
+
+
+c, h = detect_circles(testmap, 60)
+display(h, c, testmap)
